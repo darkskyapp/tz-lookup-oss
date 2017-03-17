4aa452972387f635c23df9d3b1c20ab676f8427c
J. T. L <ironwallaby@gmail.com>
2017-03-17 15:53:00 -0400

    Update rebuild script.

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
+TZ="2017a"
 
 # get data
-curl -L -O https://github.com/evansiroky/timezone-boundary-builder/releases/download/2017a/timezones.geojson.zip -O http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_urban_areas.zip -O http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_populated_places_simple.zip
+curl -L \
+  -O "https://github.com/evansiroky/timezone-boundary-builder/releases/download/$TZ/timezones.geojson.zip" \
+  -O "http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_urban_areas.zip" \
+  -O "http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_populated_places_simple.zip"
 
 # unpack tz_world_mp into a useable format. (we do this into a few tiles
 # because otherwise they're too big for Node to manage :( )
@@ -24,14 +28,14 @@ convert tz_e.tiff tz_e.pgm
 convert tz_f.tiff tz_f.pgm
 convert tz_g.tiff tz_g.pgm
 convert tz_h.tiff tz_h.pgm
-rm -rf dist tz_a.tiff tz_b.tiff tz_c.tiff tz_d.tiff tz_e.tiff tz_f.tiff tz_g.tiff tz_h.tiff tz_indexed.json
+rm -rf dist timezones.geojson.zip tz_a.tiff tz_b.tiff tz_c.tiff tz_d.tiff tz_e.tiff tz_f.tiff tz_g.tiff tz_h.tiff tz_indexed.json
 
 # unpack ne_10m_urban_areas into a useable format. (we do this into a single
 # file because even at large sizes bitmaps are pretty manageable.)
 unzip ne_10m_urban_areas.zip
 gdal_rasterize -burn 255 -te -180 -90 180 90 -ts 49152 24576 -ot Byte ne_10m_urban_areas.shp ne_10m_urban_areas.tiff
 convert ne_10m_urban_areas.tiff ne_10m_urban_areas.pbm
-rm -rf ne_10m_urban_areas.README.html ne_10m_urban_areas.VERSION.txt ne_10m_urban_areas.dbf ne_10m_urban_areas.shp ne_10m_urban_areas.shx ne_10m_urban_areas.prj ne_10m_urban_areas.tiff
+rm -rf ne_10m_urban_areas.README.html ne_10m_urban_areas.VERSION.txt ne_10m_urban_areas.dbf ne_10m_urban_areas.shp ne_10m_urban_areas.shx ne_10m_urban_areas.prj ne_10m_urban_areas.tiff ne_10m_urban_areas.zip
 
 # and, for good measure, populated places
 unzip ne_10m_populated_places_simple.zip
