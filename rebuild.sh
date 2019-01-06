bc68fc6836b41445a5c243dd7b44a4f4fbfae5be
J. T. L <ironwallaby@gmail.com>
2019-01-06 17:08:22 -0500

    misc fixes

--- rebuild.sh
+++ rebuild.sh
@@ -2,9 +2,8 @@
 set -ex
 TZ="2018i"
 
+rm -rf timezones.geojson.zip dist
 curl -L --retry 3 -C - \
   -O "https://github.com/evansiroky/timezone-boundary-builder/releases/download/$TZ/timezones.geojson.zip"
-
 unzip timezones.geojson.zip
 node pack.js | ./node_modules/.bin/uglifyjs -mc >tz.js
-rm -rf timezones.geojson.zip dist
